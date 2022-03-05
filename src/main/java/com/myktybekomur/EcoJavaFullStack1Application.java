08313dc7f1f85f9c098c14c00a1ef97131c57cb1
MyktybekOmur <mykty.omurbekov@gmail.com>
2022-03-05 10:31:54 +0300

    Initial commit

---
+++ src/main/java/com/myktybekomur/EcoJavaFullStack1Application.java
@@ -0,0 +1,13 @@
+package com.myktybekomur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoJavaFullStack1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoJavaFullStack1Application.class, args);
+    }
+
+}
